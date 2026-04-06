cb3921649e5545fb32c1dc3b10f889bac5295fcc
Nitishrohilla33 <107106399+Nitishrohilla33@users.noreply.github.com>
2026-04-06 11:31:35 +0530

    Create Bistable_system.py

---
+++ attractors/Bistable_system.py
@@ -0,0 +1,32 @@
+def x_dot(x):
+    return x - x**3
+
+# initial conditions
+t0 = 0
+x_array = np.arange(-2,2.5,0.5)
+print(x_array)
+
+# time steps
+dt = 0.0001
+steps = 20000
+
+
+# Trajectories
+x_traj = np.zeros(steps)
+t = np.zeros(steps)
+
+for x0 in x_array:
+    x = x0
+    x_traj[0] = x
+    
+    for i in range(steps-1):
+        x_traj[i+1] = x_traj[i] + x_dot(x)*dt
+        t[i+1] = t[i] + dt
+        x = x_traj[i+1]
+
+
+    x_traj = np.array(x_traj)
+    t = np.array(t)
+    
+    plt.plot(t,x_traj,label = f'x(0) = {x0}')
+    plt.legend()
