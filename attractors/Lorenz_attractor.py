1f6aeb83b7bcc35fdefae971e2d306a94fc9c3b3
Nitish Rohilla <nitishrohilla330@gmail.com>
2026-04-07 23:20:31 +0530

    added random numbers

--- attractors/Lorenz_attractor.py
+++ attractors/Lorenz_attractor.py
@@ -1,3 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
 sigma = 10
 rho = 28
 beta = 8/3
@@ -10,9 +13,10 @@ x = np.zeros(n)
 y = np.zeros(n)
 z = np.zeros(n)
 
-x[0] = 0.1*np.random.randint(0,11)
-y[0] = 0.1*np.random.randint(0,11)
-z[0] = 0.1*np.random.randint(0,11)
+np.random.seed(0)
+x[0] = np.random.normal(0,1)
+y[0] = np.random.normal(0,1)
+z[0] = np.random.normal(0,1)
 
 for i in range(n-1):
 
@@ -22,30 +26,52 @@ for i in range(n-1):
     k1z = x[i]*y[i] - beta*z[i]
 
     # k2
-    x_temp = x[i] + (2/3)*dt*k1x
-    y_temp = y[i] + (2/3)*dt*k1y
-    z_temp = z[i] + (2/3)*dt*k1z
+    x1 = x[i] + 0.5*dt*k1x
+    y1 = y[i] + 0.5*dt*k1y
+    z1 = z[i] + 0.5*dt*k1z
 
-    k2x = sigma*(y_temp-x_temp)
-    k2y = x_temp*(rho-z_temp) - y_temp
-    k2z = x_temp*y_temp - beta*z_temp
+    k2x = sigma*(y1-x1)
+    k2y = x1*(rho-z1) - y1
+    k2z = x1*y1 - beta*z1
+
+    # k3
+    x2 = x[i] + 0.5*dt*k2x
+    y2 = y[i] + 0.5*dt*k2y
+    z2 = z[i] + 0.5*dt*k2z
+
+    k3x = sigma*(y2-x2)
+    k3y = x2*(rho-z2) - y2
+    k3z = x2*y2 - beta*z2
+
+    # k4
+    x3 = x[i] + dt*k3x
+    y3 = y[i] + dt*k3y
+    z3 = z[i] + dt*k3z
+
+    k4x = sigma*(y3-x3)
+    k4y = x3*(rho-z3) - y3
+    k4z = x3*y3 - beta*z3
 
     # update
-    x[i+1] = x[i] + dt*(0.25*k1x + 0.75*k2x)
-    y[i+1] = y[i] + dt*(0.25*k1y + 0.75*k2y)
-    z[i+1] = z[i] + dt*(0.25*k1z + 0.75*k2z)
+    x[i+1] = x[i] + dt*(k1x + 2*k2x + 2*k3x + k4x)/6
+    y[i+1] = y[i] + dt*(k1y + 2*k2y + 2*k3y + k4y)/6
+    z[i+1] = z[i] + dt*(k1z + 2*k2z + 2*k3z + k4z)/6
 
-plt.figure(dpi = 1500)
+# Plot results
 plt.plot(t,x)
-
 plt.xlabel("t")
-plt.ylabel("x")
-plt.show()
+plt.ylabel("x(t)")
+plt.show()    
 
-plt.figure(dpi = 1500)
-plt.plot(y,z, linewidth = 0.8)
-plt.xlabel("y")
-plt.ylabel("z")
-plt.title("Lorenz Attractor Projection", font = 'times new roman')
+from mpl_toolkits.mplot3d import Axes3D
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+ax.plot(x, y, z, linewidth = 0.5)
+
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
+
 plt.show()
-plt.savefig()
